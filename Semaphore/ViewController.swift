ce30f5a5c3ae323c4d50be5298d472ee7e3a08f9
DenLetVar <denletvar@gmail.com>
2021-10-20 21:04:05 +0300

    Code is ready

--- Semaphore/ViewController.swift
+++ Semaphore/ViewController.swift
@@ -11,19 +11,42 @@ class ViewController: UIViewController {
     
     
     @IBOutlet var redLight: UIView!
-    
     @IBOutlet var yellowLight: UIView!
-    
     @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    var light = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redLight.layer.cornerRadius = redLight.frame.size.height
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.height
+        greenLight.layer.cornerRadius = greenLight.frame.size.width
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
     }
-
+    @IBAction func buttonPressed() {
+        startButton.setTitle("Next", for: .normal)
+        light += 1
+        switch light {
+        case 1: redLight.alpha = 1; yellowLight.alpha = 0.3; greenLight.alpha = 0.3
+        case 2: yellowLight.alpha = 1; redLight.alpha = 0.3; greenLight.alpha = 0.3
+        case 3: greenLight.alpha = 1; redLight.alpha = 0.3; yellowLight.alpha = 0.3
+        default:light = 0
+        }
+        
+        
+       
+    }
+}
+    
   
     
     
 
     
-}
+
 
